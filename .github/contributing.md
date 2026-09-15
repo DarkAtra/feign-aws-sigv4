86f55547635eb61f1281dc0776cd92c08b5015fe
DarkAtra <darkatra@gmail.com>
2026-09-15 20:12:47 +0200

    chore: replace the deprecated CDKTF with plain terraform

--- .github/contributing.md
+++ .github/contributing.md
@@ -28,39 +28,30 @@ They can also be added after the original pull request but before a merge.
 
 ## Executing Integration Tests locally
 
-In order to run Integration Tests locally, you'll have to set up the necessary infrastructure in one of your AWS Accounts.
-This can be done by applying the Terraform CDK project found in `integration-tests/infrastructure`. Here's how you do it:
+To run Integration Tests locally, you'll have to set up the necessary infrastructure in one of your AWS Accounts.
+This can be done by applying the Terraform configuration found in `integration-tests/infrastructure`. Here's how you do it:
 
 1. Make sure that no state file exists in `/integration-tests/infrastructure`.
    If it exists, see if the state file is empty. If so, delete it. Otherwise, perform Step 7 or 8.
-2. Synthesize the Terraform CDK Stack by running:
+2. Obtain Credentials for your AWS Account. This step highly depends on how the AWS Account is set up. You'll need permissions for API Gateway, Lambda and IAM.
+3. Initialize and apply the Terraform stack by running:
     ```
-    # cd into /integration-tests/infrastructure
     cd integration-tests/infrastructure
-    yarn install --immutable --immutable-cache --check-cache
-    yarn run synth
-    ```
-3. Obtain Credentials for your AWS Account. This step highly depends on how the AWS Account is set up. You'll need permissions for API Gateway, Lambda and IAM.
-4. Apply the synthesized Terraform CDK Stack by running:
-    ```
-    # cd into /integration-tests/infrastructure/cdktf.out/stacks/feign-aws-sigv4-integration-tests
-    cd cdktf.out/stacks/feign-aws-sigv4-integration-tests
     terraform init
     terraform apply
     ```
-5. Confirm with `yes` after checking the Terraform Plan output.
-6. Wait until everything is set up. This should only take a few seconds.
-7. Execute the Tests by running:
+4. Confirm with `yes` after checking the Terraform Plan output.
+5. Wait until everything is set up. This should only take a few seconds.
+6. Execute the Tests by running:
     ```
     export API_GATEWAY_URL=$(terraform output -raw api-gateway-url)
-    # cd back to /
-    cd ../../../../..
+    # cd back to the repository root
+    cd ../..
     mvn -B -ntp clean install failsafe:integration-test failsafe:verify -DapiGatewayUrl=$API_GATEWAY_URL
     ```
-8. Clean up the necessary infrastructure by running:
+7. Clean up the necessary infrastructure by running:
     ```
-    # cd into /integration-tests/infrastructure/cdktf.out/stacks/feign-aws-sigv4-integration-tests
-    cd integration-tests/infrastructure/cdktf.out/stacks/feign-aws-sigv4-integration-tests
+    cd integration-tests/infrastructure
     terraform destroy
     ```
-9. Confirm with `yes` after checking the Terraform Destroy output.
+8. Confirm with `yes` after checking the Terraform Destroy output.
---
+++ integration-tests/infrastructure/main.tf
@@ -0,0 +1,143 @@
+terraform {
+  required_version = ">= 1.16.0"
+
+  required_providers {
+    archive = {
+      source  = "hashicorp/archive"
+      version = "2.8.1"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.64.0"
+    }
+  }
+}
+
+provider "aws" {
+  region              = "eu-central-1"
+  allowed_account_ids = ["956243129466"]
+}
+
+data "aws_caller_identity" "current" {}
+
+data "archive_file" "lambda_function_asset" {
+  type        = "zip"
+  source_dir  = "${path.module}/../lambda"
+  output_path = "${path.module}/../lambda.zip"
+}
+
+resource "aws_iam_role" "lambda_function_role" {
+  name = "feign-aws-sigv4-integration-tests-lambda-function-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_function_basic_execution_attachment" {
+  role       = aws_iam_role.lambda_function_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "lambda_function" {
+  function_name    = "feign-aws-sigv4-integration-tests-lambda"
+  filename         = data.archive_file.lambda_function_asset.output_path
+  handler          = "index.handler"
+  memory_size      = 128
+  runtime          = "nodejs24.x"
+  source_code_hash = data.archive_file.lambda_function_asset.output_base64sha256
+  timeout          = 5
+  role             = aws_iam_role.lambda_function_role.arn
+}
+
+resource "aws_apigatewayv2_api" "api_gateway" {
+  name          = "feign-aws-sigv4-integration-tests-api-gateway"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "api_gateway_integration" {
+  api_id                 = aws_apigatewayv2_api.api_gateway.id
+  integration_uri        = aws_lambda_function.lambda_function.invoke_arn
+  integration_type       = "AWS_PROXY"
+  integration_method     = "POST"
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "api_gateway_route" {
+  api_id             = aws_apigatewayv2_api.api_gateway.id
+  route_key          = "$default"
+  target             = "integrations/${aws_apigatewayv2_integration.api_gateway_integration.id}"
+  authorization_type = "AWS_IAM"
+}
+
+resource "aws_lambda_permission" "api_gateway_lambda_permission" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda_function.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.api_gateway.execution_arn}/*/*"
+}
+
+resource "aws_apigatewayv2_stage" "api_gateway_stage" {
+  api_id      = aws_apigatewayv2_api.api_gateway.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+resource "aws_iam_role" "integration_test_role" {
+  name = "feign-aws-sigv4-integration-tests-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+            "token.actions.githubusercontent.com:sub" = [
+              "repo:DarkAtra/feign-aws-sigv4:ref:refs/heads/main",
+              "repo:DarkAtra/feign-aws-sigv4:ref:refs/heads/next"
+            ]
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "api_gateway_access" {
+  name = "api-gateway-access"
+  role = aws_iam_role.integration_test_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "execute-api:Invoke"
+        Resource = "${aws_apigatewayv2_api.api_gateway.execution_arn}/*/*"
+      }
+    ]
+  })
+}
+
+output "api_gateway_url" {
+  value = aws_apigatewayv2_stage.api_gateway_stage.invoke_url
+}
+
+output "integration_test_role_arn" {
+  value = aws_iam_role.integration_test_role.arn
+}
